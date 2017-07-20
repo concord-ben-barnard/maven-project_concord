90457a0cedcf61b591df82d8b260a7c0d347e126
Ben Barnard <ben.barnard@concordusa.com>
2017-07-20 09:32:46 -0500

    adding test case that errors

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,7 +1,10 @@
 package com.mulesoft.training;
 
+import static org.junit.Assert.*;
+
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -22,4 +25,11 @@ public class HelloMavenTest extends FunctionalTestCase {
     	return files;
     }
 
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+    }
+    
 }
